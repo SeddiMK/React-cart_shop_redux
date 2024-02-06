2f6a5b9ad4c94c4ac06e31a044933b19061d9e42
Max <web.egorovm@gmail.com>
2024-02-06 07:45:45 +0300

    css ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import { useEffect } from "react"
 import Main from "../components/main"
 
 
-export default function Home ()  {
+export default function Home() {
+  // При первом рендере скролим вверх
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  },[])
     return (
         <>
           <Main />

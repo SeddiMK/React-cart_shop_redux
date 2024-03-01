c6fa005da43523a7efd67921b59e6d367804233b
Max <web.egorovm@gmail.com>
2024-03-01 14:31:57 +0300

    TS_axios

--- src/store/furnitureSlice.ts
+++ src/store/furnitureSlice.ts
@@ -2,34 +2,7 @@ import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { RootState } from '.';
 
-// export type FetchFurnitureState = {
-//   sortBy,
-//       order,
-//       searchCategoryFilter,
-//       searchInpValaction,
-//       currentPage,
-// }
-
-export const fetchFurniture = createAsyncThunk(
-  'furniture/fetchFurnitureStatus',
-  async (params) => {
-    const {
-      sortBy,
-      order,
-      searchCategoryFilter,
-      searchInpValaction,
-      currentPage,
-    } = params;
-
-    const { action } = await axios.get(
-      `https://65c21d61f7e6ea59682aa9c7.mockapi.io/action_shop_furniture?limit=9&page=${currentPage}&sortBy=${sortBy}&order=${order}&search=${searchInpValaction}&filter=${searchCategoryFilter}`
-    ); //limit=должен давать бэкенд(mockapi.io- не дает всех страниц от количетва товара и массив, объект корзины)limit=6&sortBy=cost&order=asc&page=${currentPage}&search=${valFilterSearch}&rating= можно вынести в отдельный файл
-    console.log(
-      `https://65c21d61f7e6ea59682aa9c7.mockapi.io/action_shop_furniture?page=${currentPage}&sortBy=${sortBy}&order=${order}&search=${searchInpValaction}&filter=${searchCategoryFilter}`
-    );
-    return action;
-  }
-);
+// export type FetchFurnitureArgs = Record<string, string>;
 
 export type Furniture = {
   articul: string;
@@ -40,6 +13,27 @@ export type Furniture = {
   description: string;
   currency: string;
 };
+
+export const fetchFurniture = createAsyncThunk<Furniture[],Record<string, string>>('furniture/fetchFurnitureStatus',async (params) => { // (params:Record<string, string>)
+    const {
+      sortBy,
+      order,
+      searchCategoryFilter,
+      searchInpValData,
+      currentPage,
+    } = params;
+
+    const { data } = await axios.get<Furniture[]>(
+      `https://65c21d61f7e6ea59682aa9c7.mockapi.io/action_shop_furniture?limit=9&page=${currentPage}&sortBy=${sortBy}&order=${order}&search=${searchInpValData}&filter=${searchCategoryFilter}`
+    ); //limit=должен давать бэкенд(mockapi.io- не дает всех страниц от количетва товара и массив, объект корзины)limit=6&sortBy=cost&order=asc
+    console.log(
+      `https://65c21d61f7e6ea59682aa9c7.mockapi.io/action_shop_furniture?page=${currentPage}&sortBy=${sortBy}&order=${order}&search=${searchInpValData}&filter=${searchCategoryFilter}`
+    );
+    return data; // as Furniture[];
+  }
+);
+
+
 
 interface FurnitureSliceState {
   items: Furniture[];

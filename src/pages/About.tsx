bc527e393ca79d3402c20cc522d0a5131da77f23
Max <web.egorovm@gmail.com>
2024-03-14 15:51:43 +0300

    about_Page

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,32 @@
 const About: React.FC = () => {
   return (
     <>
-      <div>About US</div>
+      <section className="about">
+        <br />
+        <div className="about__title">About US</div>
+        <br />
+        <div className="about__text">
+          <p>
+            This application was developed using the React library, and same
+            Redux-toolkit,Configure-store, Axios,TypeScript, JavaScript, HTML,
+            SCSS, Mobile-First.
+          </p>
+          <br />
+          <p>
+            Данное приложение разаработано с применением библиотеки React, а так
+            же Redux-toolkit, Axios,TypeScript, JavaScript, HTML, SCSS,
+            Mobile-First.
+          </p>
+          <br />
+          <p>My contact:</p>
+          <p>Связь со мной:</p>
+          <br />
+          <p>Mail: web.egorovm@gmail.com</p>
+          <p>GitHub: SeddiMK</p>
+          <p>Discord: seddi4381</p>
+        </div>
+        <br />
+      </section>
     </>
   );
 };

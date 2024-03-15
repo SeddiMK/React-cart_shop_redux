6121123a54958ebc33df86b6de699bdc866ff920
Max <web.egorovm@gmail.com>
2024-03-15 14:25:28 +0300

    about_my_contact

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 const About: React.FC = () => {
   return (
     <>
@@ -21,9 +22,37 @@ const About: React.FC = () => {
           <p>My contact:</p>
           <p>Связь со мной:</p>
           <br />
-          <p>Mail: web.egorovm@gmail.com</p>
-          <p>GitHub: SeddiMK</p>
-          <p>Discord: seddi4381</p>
+
+          <p>
+            Mail:{' '}
+            <a
+              href="https://mail.google.com/mail/u/1/?view=cm&fs=1&to=web.egorovm@gmail.com&tf=1"
+              target="_blank"
+              rel="noreferrer">
+              web.egorovm@gmail.com
+            </a>
+          </p>
+          <br />
+          <p>
+            GitHub:{' '}
+            <a
+              href="https://github.com/SeddiMK"
+              target="_blank"
+              rel="noreferrer">
+              SeddiMK
+            </a>
+          </p>
+          <br />
+          <p>
+            Skype:{' '}
+            <a
+              href="skype:live:.cid.985f030235657018?add"
+              target="_blank"
+              rel="noreferrer">
+              Maksim Egorov
+            </a>
+          </p>
+          <br />
         </div>
         <br />
       </section>
